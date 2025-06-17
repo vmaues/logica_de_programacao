0cc994eb7caa31ddabf2b68e8bbf53cbc5075184
Gleison1784 <joaomaues11@gmail.com>
2025-06-17 10:47:05 -0400

    a
    a

---
+++ hellow.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public class main {
+    public static void main(string[] args)
+    system.out.printin("hello, world!");
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Digite o nome do jogador")
+
+// constante e variaveis
+
+var nome = "joão "
+const saudacao = "Bem vindo "
+const entrada = "Entrou no servidor"
+
+console.log(saudacao + nome)
+console.log(nome + entrada)
